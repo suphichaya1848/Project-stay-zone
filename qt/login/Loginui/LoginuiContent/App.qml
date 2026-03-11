c22ffb47e62abee262bb1bae48073cdd018cb6dc
suphichaya1848 <suphichaya.1848@gmail.com>
2026-03-12 01:52:26 +0700

    final1

---
+++ qt/login/Loginui/LoginuiContent/App.qml
@@ -0,0 +1,16 @@
+import QtQuick
+import Loginui
+
+Window {
+    width: mainScreen.width
+    height: mainScreen.height
+
+    visible: true
+    title: "Loginui"
+
+    Screen01 {
+        id: mainScreen
+    }
+
+}
+
---
+++ qt/login/build/Desktop_Qt_6_9_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_login_FILE C:/Users/suphi/OneDrive/Desktop/project/qt/login/build/Desktop_Qt_6_9_3_MinGW_64_bit-Debug/login.exe)
+set(__QT_DEPLOY_TARGET_login_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_login_RUNTIME_DLLS C:/Qt/6.9.3/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.3/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.3/mingw_64/bin/Qt6Core.dll)
